4be33bb4c6bce7c1008bb119a0fd4a2959d88c65
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-11-25 20:30:33 +0700

    first commit

---
+++ Data/Entities/Gadgets.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webappmssql.Data.Entities
+{
+    public class Gadgets
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Brand { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public string Type { get; set; }
+    }
+}
